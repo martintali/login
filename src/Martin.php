fade478f31f90f55f966b5d2b2ed11e8530be6ee
wo <martin.daniel@wobiz.com>
2016-02-19 20:20:49 -0300

    first commit

---
+++ src/Martin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martindaniel
+ * Date: 2/19/16
+ * Time: 8:12 PM
+ */
+
+namespace MartinTali\Login;
+
+
+class Martin
+{
+
+}
---
+++ tests/MartinTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martindaniel
+ * Date: 2/19/16
+ * Time: 8:15 PM
+ */
+
+namespace MartinTali\Login\Login;
+
+
+class MartinTest
+{
+
+}
